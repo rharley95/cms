2ac2d7c2e7dd105d34c804f3649dc532fed84f73
Romina Painter <romina.painter@stukent.com>
2019-09-27 22:46:13 -0600

    finished the tutorial! we dont have real data yet

--- contacts/contact-list/contact-list.component.ts
+++ contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-list',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactListComponent implements OnInit {
 
+  contacts: Contact[] = [
+    // tslint:disable-next-line: max-line-length
+    new Contact('1', 'Bro. Jackson', 'jacksonk@byui.edu', '208-496-3771', 'https://web.byui.edu/Directory/Employee/jacksonk.jpg', null),
+    // tslint:disable-next-line: max-line-length
+    new Contact('2', 'Bro. Barzee', 'barzeer@byui.edu', '208-496-3768', 'https://web.byui.edu/Directory/Employee/barzeer.jpg', 'simply a test')
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
